1b9a240e9628055e48594775d972624dd4e7696c
David Castillo <dc4stillo89@gmail.com>
2016-01-11 13:02:19 +0100

    Added Clase Carta y .gitignore

---
+++ src/cartas/Carta.java
@@ -0,0 +1,55 @@
+/*
+* Clase Carta.java
+*/
+package cartas;
+
+/**
+* @author @leoacal
+*/
+public class Carta {
+    // Atributos
+    private char baraja;
+    private int numero;
+    private String palo;
+    
+    /**
+    * Constructor
+    * @param baraja Tipo de baraja
+    * @param numero Ordinal de la carta
+    * @param palo Palo al que pertenece 
+    */
+    public Carta( char baraja, int numero, String palo) {
+        this.baraja = baraja;
+        this.numero = numero;
+        this.palo = palo;
+        }   // Cierra constructor
+     
+    public char getBaraja() {
+        return baraja;
+    }
+
+    public void setBaraja(char baraja) {
+        this.baraja = baraja;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+   
+    @Override
+    public String toString() { 
+            return numero + " de " + palo;
+    }   // Cierra metodo toString
+} // Cierra clase
